5c175756c6ae5e22130a497547f3d61b1aa4dd6f
David Corticchiato <david.corticchiato@gmail.com>
2020-02-23 18:01:38 -0500

    scale only if screen is too small

--- main.lua
+++ main.lua
@@ -2,10 +2,16 @@ local GameScreen = require("lamavolley.screen.game")
 local TitleScreen = require("lamavolley.screen.title")
 
 local screen = GameScreen()
+
+local maxWidth = love.graphics.getWidth()
+local maxHeight = love.graphics.getHeight()
+
+local needScale = maxWidth < 1408 or maxHeight < 1024
+
 local canvas = love.graphics.newCanvas(1408, 1024)
 
 function love.load()
-  love.window.setMode(1408 / 2, 1024 / 2)
+  love.window.setMode(needScale and 1408 / 2 or 1408, needScale and 1024 / 2 or 1024)
 end
 
 function love.update(dt)
@@ -13,11 +19,17 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.setCanvas(canvas)
-  love.graphics.clear()
+  if needScale then
+    love.graphics.setCanvas(canvas)
+    love.graphics.clear()
+  end
+
   screen:draw()
-  love.graphics.setCanvas()
-  love.graphics.draw(canvas, 0, 0, 0, 0.5, 0.5)
+
+  if needScale then
+    love.graphics.setCanvas()
+    love.graphics.draw(canvas, 0, 0, 0, 0.5, 0.5)
+  end
 end
 
 function love.keypressed(key, unicode)
